f5af07431d4ab8800479bc21686b4840f107209c
mcpiroman <38111589+mcpiroman@users.noreply.github.com>
2020-07-03 11:32:18 +0200

    Update Versions.kt

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,5 +1,5 @@
 object Versions {
-    const val protobuf = "3.9.0"
+    const val protobuf = "3.12.2"
     const val commonProto = "1.16.0"
     const val grpc = "1.23.0"
     const val kotlin = "1.3.61"
